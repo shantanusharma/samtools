4d897708601d3ba7d24de7b4053e3e4ca7596a42
James Bonfield <jkb@sanger.ac.uk>
2026-07-01 09:57:53 +0100

    Avoid sed warnings in regression.sh
    Solaris claims "sed: Missing newline at end of file standard input".
    It's true, but harmless.  Nonetheless the workaround makes is also
    harmless to other OSes and it makes it easier to see genuine problems.
    
    Signed-off-by: James Bonfield <jkb@sanger.ac.uk>

--- test/regression.sh
+++ test/regression.sh
@@ -115,14 +115,14 @@ regtest() {
             test_iter=0
             if [ "`expr \"$cmd\" : '.*\$fmt'`" != 0 -a "$samtools" != "./samtools-0.1.19" ]
             then
-                _cmd=`printf '%s' "$cmd" | sed 's/\$fmt/bam/'`
+                _cmd=`printf '%s\n' "$cmd" | sed 's/\$fmt/bam/'`
                 run_test $p $o $_cmd
                 #_cmd=`printf '%s' "$cmd" | sed 's/\$fmt/sam/'`
                 #run_test $p $o $_cmd
-                _cmd=`printf '%s' "$cmd" | sed 's/\$fmt/cram/'`
+                _cmd=`printf '%s\n' "$cmd" | sed 's/\$fmt/cram/'`
                 run_test $p $o $_cmd
             else
-                _cmd=`printf '%s' "$cmd" | sed 's/\$fmt/bam/'`
+                _cmd=`printf '%s\n' "$cmd" | sed 's/\$fmt/bam/'`
                 run_test $p $o $_cmd
             fi
             ;;
